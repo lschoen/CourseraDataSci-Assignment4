6276d848cbb6f87f2614caed2f34d2c5e7bb6a24
lschoen <luke.schoen@gmail.com>
2019-08-30 12:29:48 +1000

    cleaned up comments

--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,7 @@
-# # One of the most exciting areas in all of data science right now is wearable computing 
-# - see for example this article . Companies like Fitbit, Nike, and Jawbone Up are racing 
-# to develop the most advanced algorithms to attract new users. The data linked to from the 
-# course website represent data collected from the accelerometers from the Samsung Galaxy S 
-# smartphone. A full description is available at the site where the data was obtained: 
-# #     
-# #     http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
-# # 
-# # Here are the data for the project:
-# #     
-# #     https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
-# # 
-# # You should create one R script called run_analysis.R that does the following.
+# #  The is the course assignment for Coursera "Getting and Cleaning Data" class.
+# #  
+# #  See README.md and CodeBook.md for additional information
+# #  Per the instructions, this script does the following:
 # # 
 # # - Merges the training and the test sets to create one data set.
 # # - Extracts only the measurements on the mean and standard deviation for each measurement.
